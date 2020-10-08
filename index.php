4da68685acff6ab0d47377a03d47b6eeea14c170
sayed <sallamfcis@gmail.com>
2020-10-08 17:30:24 +0200

    welcome to site

---
+++ index.php
@@ -0,0 +1,112 @@
+
+<html>
+ <head>
+  <title>Data Production Of  Workshop101</title>
+  <script src="include/jquery-1.12.4.js"></script>
+  <link rel="stylesheet" href="include/bootstrap.min.css" />
+  <script src="include/jquery.dataTables.min.js"></script>
+  <script src="include/dataTables.bootstrap.min.js"></script>
+  <link rel="stylesheet" href="include/bootstrap-datepicker.css" />
+  <script src="include/bootstrap-datepicker.js"></script>
+  <style>
+   body
+   {
+    margin:0;
+    padding:0;
+    background-color:#f1f1f1;
+   }
+   .box
+   {
+    width:1270px;
+    padding:20px;
+    background-color:#fff;
+    border:1px solid #ccc;
+    border-radius:5px;
+    margin-top:25px;
+   }
+  </style>
+  
+
+ </head>
+ <body>
+  <div class="container box">
+   <h1 align="center">Data Production Of  Workshop101</h1>
+   <br />
+   <div class="table-responsive">
+    <br />
+    <div class="row">
+     <div class="input-daterange">
+      <div class="col-md-4">
+       <input type="text" name="start_date" id="start_date" class="form-control" />
+      </div>
+      <div class="col-md-4">
+       <input type="text" name="end_date" id="end_date" class="form-control" />
+      </div>      
+     </div>
+     <div class="col-md-4">
+      <input type="button" name="search" id="search" value="Search" class="btn btn-info" />
+     </div>
+    </div>
+    <br />
+    <table id="order_data" class="table table-bordered table-striped">
+     <thead>
+      <tr>
+       <th> ID</th>
+       <th>Department</th>
+       <th>Item</th>
+       <th>Number Of Item</th>
+       <th> Date</th>
+      </tr>
+     </thead>
+    </table>
+    
+   </div>
+  </div>
+ </body>
+</html>
+
+
+
+<script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+ 
+ $('.input-daterange').datepicker({
+  todayBtn:'linked',
+  format: "yyyy-mm-dd",
+  autoclose: true
+ });
+
+ fetch_data('no');
+
+ function fetch_data(is_date_search, start_date='', end_date='')
+ {
+  var dataTable = $('#order_data').DataTable({
+   "processing" : true,
+   "serverSide" : true,
+   "order" : [],
+   "ajax" : {
+    url:"fetch.php",
+    type:"POST",
+    data:{
+     is_date_search:is_date_search, start_date:start_date, end_date:end_date
+    }
+   }
+  });
+ }
+
+ $('#search').click(function(){
+  var start_date = $('#start_date').val();
+  var end_date = $('#end_date').val();
+  if(start_date != '' && end_date !='')
+  {
+   $('#order_data').DataTable().destroy();
+   fetch_data('yes', start_date, end_date);
+  }
+  else
+  {
+   alert("Both Date is Required");
+  }
+ }); 
+ 
+});
+</script>
